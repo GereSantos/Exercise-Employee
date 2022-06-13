ae2addc7bbb746b4b74506e84961549064d1763b
Geremias Santos <Geremias99999@gmail.com>
2022-06-13 09:40:31 -0300

    Exercise Employee

---
+++ Exercise Employee/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise_Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programa que ler Nº de funcionário, horas trabalhadas e quanto recebe por hora
+            //Calculando também o salário desse funcionário
+            int nºFunc = int.Parse(Console.ReadLine());
+            int htraba = int.Parse(Console.ReadLine());
+            double vlphora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double salario=0.0;
+
+            salario = htraba * vlphora;
+            Console.WriteLine("Number: " + nºFunc);
+            Console.WriteLine("Salary: " + salario.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
